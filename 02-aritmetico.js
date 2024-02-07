78bf517a2ce9b4d96a786e27fbecafc4fb37781d
eloá <francodosreissilvae@gmail.com>
2024-02-07 17:05:29 -0300

    atividades finalizadas

--- 02-aritmetico.js
+++ 02-aritmetico.js
@@ -4,13 +4,13 @@ console.log("A soma entre 8 e 7 é:",8 + 7)
 
 //efetuar a soma de dois numeros  informados por você (var nome_variavel = 12)
 
-var x = 7
-var y =7 
+ var x = 7
+ var y =7 
 
-console.log ("A soma entre", x, "e", y, "é:", x + y )
-var soma = x = y ;
-var subtracao = x -y;
-var multiplicacao = x * y
+ console.log ("A soma entre", x, "e", y, "é:", x + y )
+ var soma = x = y ;
+ var subtracao = x -y;
+ var multiplicacao = x * y
 var divisao = x / y; 
 
 console.log(""); //outra forma de pular linha 
@@ -19,16 +19,18 @@ console.log("Soma = ", soma);
 console.log("subtracao = ", subtracao);
 console.log("multiplicacao = ", multiplicacao);
 console.log("divisao = ", divisao);
-
 //Regras para nomear identificadores (var, const ou let)!
 //podes começar com A-Z, a-z, _ ou $
 
-var minhavariavel = 20;
-var outravariavel = 20;
-const _minhaconstante = 3.1514;
-var $minhavariavel = 11;
+ var minhavariavel = 20;
+ var outravariavel = 20;
+ const _minhaconstante = 3.1514;
+ var $minhavariavel = 11;
 
-var v1 =10
-var v2 = 3 
-console.log("................................");
-console.log("Valores: v1" = , v1, * | v2 =* 
+ var v1 =10
+ var v2 = 3 
+console.log("...........................");
+console.log("valores v1=", v1, "| v2=",v2);
+console.log("...........................");
+console.log("resto de divisão inteira v1 % v2=",v1 **v2);
+console.log("exponenciacao v1**v2 (v1 elevado a v2)=", v1**v2);
---
+++ 03somandotexto.js
@@ -0,0 +1,30 @@
+//somando textos 
+var texto1 = "olá";
+var texto2 = "bom dia";
+
+ console.log(texto1 + texto2);
+ console.log(texto1 + "," + texto2);
+
+ //procedencia de operadores 
+ var x = 22
+ var y = 5 
+ console.log("x =" , x,"| y = ", y)
+
+ var resultado = x + y - 3
+ console.log(resultado)
+ 
+var resultado = x + y * 3
+console.log(resultado)
+
+var resultado = (x + y) *3 
+console.log(resultado)
+
+var resultado = (x + y) / 3
+console.log(resultado)
+
+var resultado = (x/2 + 3*y + (x- 10))/2
+console.log (resultado)
+
+
+
+ 
