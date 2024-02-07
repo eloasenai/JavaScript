1c688ea052901b6f217e2554e38fc850c1a5d8cb
eloá <francodosreissilvae@gmail.com>
2024-02-07 11:26:12 -0300

    JavaScript- basico

---
+++ 01-variavel.js
@@ -0,0 +1,11 @@
+//Criando variaveis em "var" (var nome.variaveis = valor>;)
+
+var nome = "Eloá"; //var cria variavel global
+var idade = 17
+
+console.log("Meu nome é:", nome, "e tenho" ,idade,"anos !");
+console.log("Agora fiz aniversário ! tenho", idade + 1,"anos!");
+
+//aumentando automaticamente +1 (automaticamente)
+idade++ //idade= idade + 1;
+console.log("Agora fiz aniversario! tenho", idade +1,  "anos!"); 
---
+++ 02-aritmetico.js
@@ -0,0 +1,29 @@
+//utilizando operadores aritmeticos com JavaScrip
+
+console.log("A soma entre 8 e 7 é:",8 + 7)
+
+//efetuar a soma de dois numeros  informados por você (var nome_variavel = 12)
+
+var x = 7
+var y =7 
+
+console.log ("A soma entre", x, "e", y, "é:", x + y )
+var soma = x = y ;
+var subtracao = x -y;
+var multiplicacao = x * y
+var divisao = x / y; 
+
+console.log(""); //outra forma de pular linha 
+
+console.log("Soma = ", soma);
+console.log("subtracao = ", subtracao);
+console.log("multiplicacao = ", multiplicacao);
+console.log("divisao = ", divisao);
+
+//Regras para nomear identificadores (var, const ou let)!
+//podes começar com A-Z, a-z, _ ou $
+
+var minhavariavel = 20;
+var outravariavel = 20;
+const _minhaconstante = 3.1514;
+var $minhavariavel = 11;
